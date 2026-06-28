dcf1673fad45ec439d76aaa499772bb4c6ff9e53
dbarzin <dbarzin@gmail.com>
2026-06-28 11:56:28 +0200

    fix migration from SQLite

--- database/migrations/2026_06_28_000000_drop_unique_name_from_backups_table.php
+++ database/migrations/2026_06_28_000000_drop_unique_name_from_backups_table.php
@@ -2,25 +2,23 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     public function up(): void
     {
-        $columns = collect(DB::select('DESCRIBE backups'))->pluck('Field');
-        $indexes = collect(DB::select("SHOW INDEX FROM backups"))->pluck('Key_name');
+        $indexNames = collect(Schema::getIndexes('backups'))->pluck('name');
 
-        Schema::table('backups', function (Blueprint $table) use ($columns, $indexes) {
-            if ($indexes->contains('backups_name_unique')) {
+        Schema::table('backups', function (Blueprint $table) use ($indexNames) {
+            if ($indexNames->contains('backups_name_unique')) {
                 $table->dropUnique('backups_name_unique');
             }
-            if ($columns->contains('logical_server_id')) {
+            if (Schema::hasColumn('backups', 'logical_server_id')) {
                 $table->dropForeign(['logical_server_id']);
                 $table->dropColumn('logical_server_id');
             }
-            if ($columns->contains('storage_device_id')) {
+            if (Schema::hasColumn('backups', 'storage_device_id')) {
                 $table->dropForeign(['storage_device_id']);
                 $table->dropColumn('storage_device_id');
             }
